735cf860b194d076be96259cc18dc663514209bc
Ilya Trufakin <i.trufakin@gmail.com>
2024-10-13 01:48:15 +0800

    V0.04

--- main.cpp
+++ main.cpp
@@ -1,43 +1,90 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main() {
-    // Параметры для GStreamer камеры
-    std::string pipeline = "videotestsrc ! video/x-raw, width=640, height=480, framerate=30/1 ! videoconvert ! appsink";
+int main()
+{
+    int sensor_id0 = 0; // 0 для CAM0 и 1 для CAM1
+    int sensor_id1 = 1; // 0 для CAM0 и 1 для CAM1
+    int framerate = 60; // Установите нужный фреймрейт
+
+    std::string pipeline0 = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id0) +
+                            " ! video/x-raw(memory:NVMM),width=1920,height=1080,framerate=" + std::to_string(framerate) + "/1" +
+                            " ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! appsink";
+
+    std::string pipeline1 = "nvarguscamerasrc sensor-id=" + std::to_string(sensor_id1) +
+                            " ! video/x-raw(memory:NVMM),width=1920,height=1080,framerate=" + std::to_string(framerate) + "/1" +
+                            " ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! appsink";
 
     std::cout << "Инициализация захвата видео..." << std::endl;
-    cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
+    cv::VideoCapture cap0(pipeline0, cv::CAP_GSTREAMER);
+    cv::VideoCapture cap1(pipeline1, cv::CAP_GSTREAMER);
 
-    if (!cap.isOpened()) {
-        std::cerr << "Ошибка: не удалось открыть видеопоток!" << std::endl;
-    } else {
-        std::cout << "Видеопоток открыт успешно!" << std::endl;
+    if (!cap0.isOpened())
+    {
+        std::cerr << "Ошибка: не удалось открыть видеопоток с камеры 0!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Видеопоток с камеры 0 открыт успешно!" << std::endl;
+    }
+
+    if (!cap1.isOpened())
+    {
+        std::cerr << "Ошибка: не удалось открыть видеопоток с камеры 1!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Видеопоток с камеры 1 открыт успешно!" << std::endl;
     }
 
     // Создание окон
     std::cout << "Создание окон..." << std::endl;
-    cv::namedWindow("Камера", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Камера0", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Камера1", cv::WINDOW_AUTOSIZE);
     cv::namedWindow("Параметры", cv::WINDOW_AUTOSIZE);
 
-    cv::Mat frame;
+    cv::Mat frame0;
+    cv::Mat frame1;
 
-    while (true) {
-        if (cap.isOpened()) {
-            cap >> frame;
-            if (frame.empty()) {
+    while (true)
+    {
+        if (cap0.isOpened())
+        {
+            cap0 >> frame0;
+            if (frame0.empty())
+            {
                 std::cerr << "Ошибка: пустой кадр!" << std::endl;
-                frame = cv::Mat::zeros(480, 640, CV_8UC3); // Создание пустого изображения
+                frame0 = cv::Mat::zeros(480, 640, CV_8UC3); // Создание пустого изображения
             }
-        } else {
+        }
+        else
+        {
             // Используем пустое изображение, если видеопоток не открыт
-            frame = cv::Mat::zeros(480, 640, CV_8UC3); // Черное изображение
+            frame0 = cv::Mat::zeros(480, 640, CV_8UC3); // Черное изображение
+        }
+
+        if (cap1.isOpened())
+        {
+            cap1 >> frame1;
+            if (frame1.empty())
+            {
+                std::cerr << "Ошибка: пустой кадр!" << std::endl;
+                frame1 = cv::Mat::zeros(480, 640, CV_8UC3); // Создание пустого изображения
+            }
+        }
+        else
+        {
+            // Используем пустое изображение, если видеопоток не открыт
+            frame1 = cv::Mat::zeros(480, 640, CV_8UC3); // Черное изображение
         }
 
         // Наложение текста на видео
-        cv::putText(frame, "Тестовый источник видео", cv::Point(50, 50), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 0, 0), 2);
-
+        cv::putText(frame0, "Тестовый источник видео0", cv::Point(50, 50), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 0, 0), 2);
+        cv::putText(frame1, "Тестовый источник видео1", cv::Point(50, 50), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(255, 0, 0), 2);
         // Вывод видео
-        cv::imshow("Камера", frame);
+
+        cv::imshow("Камера0", frame0);
+        cv::imshow("Камера1", frame1);
 
         // Параметры
         cv::Mat paramWindow(200, 400, CV_8UC3, cv::Scalar(255, 255, 255));
@@ -45,12 +92,18 @@ int main() {
         cv::imshow("Параметры", paramWindow);
 
         // Ожидание нажатия клавиши
-        if (cv::waitKey(30) >= 0) break;
+        if (cv::waitKey(30) >= 0)
+            break;
     }
 
     // Освобождение ресурсов
-    if (cap.isOpened()) {
-        cap.release();
+    if (cap0.isOpened())
+    {
+        cap0.release();
+    }
+     if (cap1.isOpened())
+    {
+        cap1.release();
     }
     cv::destroyAllWindows();
     return 0;
